c2315e6ebf5423c42adeb2e6abfbce52a62702d1
PriscilaBrandao <priscilabrandao1987@gmail.com>
2018-12-13 22:00:44 -0200

    segundo commit

--- src/main/java/com/fieb/senai/entidades/Cidade.java
+++ src/main/java/com/fieb/senai/entidades/Cidade.java
@@ -5,10 +5,69 @@
  */
 package com.fieb.senai.entidades;
 
+import java.util.Objects;
+
 /**
  *
  * @author Aluno
  */
 public class Cidade {
+    private String nome;
+    private Estado estado;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
     
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+        
+        
+    }
+
+    public Cidade() {
+    }
+
 }
+    
+
+    
+
--- src/main/java/com/fieb/senai/entidades/Cliente.java
+++ src/main/java/com/fieb/senai/entidades/Cliente.java
@@ -5,10 +5,108 @@
  */
 package com.fieb.senai.entidades;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  *
  * @author Aluno
  */
 public class Cliente {
+    private String nome;
+    private Integer matricula;
+    private Integer idade;
+    private Endereco endereço;
+    private Set<String> telefone = new HashSet<>();
+
+    public Cliente(String nome, Integer matricula, Integer idade) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Endereco getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(Endereco endereço) {
+        this.endereço = endereço;
+    }
+
+    public Set<String> getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Set<String> telefone) {
+        this.telefone = telefone;
+    }
+
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + Objects.hashCode(this.nome);
+        hash = 23 * hash + Objects.hashCode(this.matricula);
+        hash = 23 * hash + Objects.hashCode(this.idade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        return true;
+        
+        
+    }
+
+    public Cliente() {
+    }
+    
+    
     
 }
